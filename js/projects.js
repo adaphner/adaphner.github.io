d34ac61ccafc380229b4ac903aa0cff4b7f7dc45
Daphner Aulibrice <aulibriced@gmail.com>
2022-09-04 16:23:27 -0400

    optimized random color palette generator code files

--- js/projects.js
+++ js/projects.js
@@ -21,15 +21,21 @@ function updateSubPage(subPage, subNav) {
     // subNav.style.borderTop = '1px solid #204666';
 }
 
+//to initialize background color and display initial hex values
+function displayInitialColors() {
+    const bgColor = ['#35788C', '#6398AA', '#5E8379', '#79A1C7', '#204666', '#3F6B9B'];
 
-//Change background color
-function changeBackgroundColor() {
-    const bgColor = ['#222222'];
-    const rand = Math.floor(Math.random() * bgColor.length);
-    return bgColor[rand];
+    const hexData = document.querySelectorAll('.hex-number');
+
+    hexData.forEach((el, index) => {
+        const initialColor = bgColor[index];
+        el.parentElement.style.backgroundColor = initialColor;
+        el.innerText = initialColor;
+    });
 }
+displayInitialColors();
 
-// to generate random color palette
+// to generate random hex color values
 function randomColors() {
     let hex = '#';
     const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
@@ -40,40 +46,17 @@ function randomColors() {
     return hex;
 }
 
-const bgColorButton = document.getElementById('bg-color-button').addEventListener('click', () => {
+// to display new hex values when button is clicked
+document.getElementById('bg-color-button').addEventListener('click', () => {
 
-    //Hex color generator one
-    const hexColorOne = randomColors();
-    document.getElementById('hex-one').style.backgroundColor = hexColorOne;
-    document.getElementById('hex-one-text').innerText = hexColorOne;
+    const hexData = document.querySelectorAll('.hex-number');
 
-    //Hex color generator two
-    const hexColorTwo = randomColors();
-    document.getElementById('hex-two').style.backgroundColor = hexColorTwo;
-    document.getElementById('hex-two-text').innerText = hexColorTwo;
-
-    //Hex color generator three
-    const hexColorThree = randomColors();
-    document.getElementById('hex-three').style.backgroundColor = hexColorThree;
-    document.getElementById('hex-three-text').innerText = hexColorThree;
-
-    //Hex color generator four
-    const hexColorFour = randomColors();
-    document.getElementById('hex-four').style.backgroundColor = hexColorFour;
-    document.getElementById('hex-four-text').innerText = hexColorFour;
-
-    //Hex color generator five
-    const hexColorFive = randomColors();
-    document.getElementById('hex-five').style.backgroundColor = hexColorFive;
-    document.getElementById('hex-five-text').innerText = hexColorFive;
-
-    //Hex color generator six
-    const hexColorSix = randomColors();
-    document.getElementById('hex-six').style.backgroundColor = hexColorSix;
-    document.getElementById('hex-six-text').innerText = hexColorSix;
-
+    hexData.forEach(el => {
+        const hexColor = randomColors();
+        el.parentElement.style.backgroundColor = hexColor;
+        el.innerText = hexColor;
+    });
 });
-
 
 //Calculator function
 function Calculator(output) {
