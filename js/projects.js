3c532a672210677d33875fb1e349e9f53e58757e
Daphner Aulibrice <aulibriced@gmail.com>
2022-09-10 02:29:10 -0400

    added password generator app

--- js/projects.js
+++ js/projects.js
@@ -257,7 +257,6 @@ prevQuestionBtn.addEventListener("click", () => {
         .catch((err) => console.log("Something went wrong. Try again later." + err));
 });
 
-
 //to fetch random quotes
 function getQuotes() {
     fetch("/files/quotes.json")
@@ -273,3 +272,29 @@ getQuotes();
 setInterval(getQuotes, 100000);
 
 document.getElementById("quote-btn").addEventListener("click", () => getQuotes());
+
+// password generator
+const generateRandomInt = (max, min) => Math.round(Math.random() * (max - min + 1)) + min;
+function passwordGenerator() {
+    let passwordLength = generateRandomInt(10, 15);
+    let password = '';
+    const characters = 'abcdefghijklmNOPQRSTUVWXYZ0123456789~!@#$%^&*()ABCDEFGHIJKLMnopqrstuvwxyz';
+    for (let i = 0; i < passwordLength; i++) {
+        const random = Math.round(Math.random() * characters.length);
+        password += characters.charAt(random);
+    }
+    return password;
+}
+
+function displayPassword() {
+    const genPassBtn = document.getElementById('btn-password');
+    genPassBtn.addEventListener('click', () => {
+        let password = passwordGenerator();
+        const passText = document.getElementById('pass-text');
+        const passLen = document.getElementById('pass-len');
+        passText.innerText = password;
+        passLen.innerText = password.length;
+
+    });
+}
+displayPassword();
