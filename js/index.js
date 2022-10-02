8a9ccca6e26bfed78ba08d90649a0a437cf0ba89
Daphner Aulibrice <aulibriced@gmail.com>
2022-10-02 08:15:35 -0400

    updated nav bar and overall layout

--- js/index.js
+++ js/index.js
@@ -23,15 +23,7 @@ function updatePage(page, nav) {
 
 window.onscroll = () => {
 
-    //to change subnav display when scrolling pass 250px
-    const subNav = document.getElementById('projects-sub-nav');
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-        subNav.style.display = 'none';
-    } else {
-        subNav.style.display = 'flex';
-    }
-
-    //to change nav and subnav display when scrolling pass 450px
+    //to change nav display when scrolling pass 450px
     const mainNav = document.getElementById('navigation');
     if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
         mainNav.style.display = 'none';
--- js/projects.js
+++ js/projects.js
@@ -1,62 +1,3 @@
-//to process and update navigation tabs
-document.querySelectorAll('.projects-tab').forEach(subNav => subNav.addEventListener('click', (e) => {
-    const subPage = e.target.innerText;
-    updateSubPage(subPage, subNav);
-}));
-
-//to display Basics subPage by default
-document.getElementById('default-projects').click();
-
-function updateSubPage(subPage, subNav) {
-
-    document.querySelectorAll('.projects-content').forEach((subPage) => subPage.style.display = 'none');
-    document.querySelectorAll('.projects-tab').forEach((subNav) => {
-        subNav.style.backgroundColor = '#ffffff';
-        subNav.style.borderBottom = '1px solid #ffffff';
-    });
-
-    const showPage = document.getElementById(subPage);
-    showPage.style.display = 'flex';
-    subNav.style.borderBottom = '1px solid #204666';
-}
-
-//to initialize background color and display initial hex values
-function displayInitialColors() {
-    const bgColor = ['#35788C', '#6398AA', '#5E8379', '#79A1C7', '#204666', '#3F6B9B'];
-
-    const hexData = document.querySelectorAll('.hex-number');
-
-    hexData.forEach((el, index) => {
-        const initialColor = bgColor[index];
-        el.parentElement.style.backgroundColor = initialColor;
-        el.innerText = initialColor;
-    });
-}
-displayInitialColors();
-
-// to generate random hex color values
-function randomColors() {
-    let hex = '#';
-    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-    for (let i = 0; i < 6; i++) {
-        const rand = Math.floor(Math.random() * hexValues.length);
-        hex += hexValues[rand];
-    }
-    return hex;
-}
-
-// to display new hex values when button is clicked
-document.getElementById('bg-color-button').addEventListener('click', () => {
-
-    const hexData = document.querySelectorAll('.hex-number');
-
-    hexData.forEach(el => {
-        const hexColor = randomColors();
-        el.parentElement.style.backgroundColor = hexColor;
-        el.innerText = hexColor;
-    });
-});
-
 //Calculator function
 function Calculator(output) {
     let outputValues = output;
@@ -156,6 +97,45 @@ function calculatorDriver() {
 }
 calculatorDriver();
 
+
+//to initialize background color and display initial hex values
+function displayInitialColors() {
+    const bgColor = ['#35788C', '#6398AA', '#5E8379', '#79A1C7', '#204666', '#3F6B9B'];
+
+    const hexData = document.querySelectorAll('.hex-number');
+
+    hexData.forEach((el, index) => {
+        const initialColor = bgColor[index];
+        el.parentElement.style.backgroundColor = initialColor;
+        el.innerText = initialColor;
+    });
+}
+displayInitialColors();
+
+// to generate random hex color values
+function randomColors() {
+    let hex = '#';
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    for (let i = 0; i < 6; i++) {
+        const rand = Math.floor(Math.random() * hexValues.length);
+        hex += hexValues[rand];
+    }
+    return hex;
+}
+
+// to display new hex values when button is clicked
+document.getElementById('bg-color-button').addEventListener('click', () => {
+
+    const hexData = document.querySelectorAll('.hex-number');
+
+    hexData.forEach(el => {
+        const hexColor = randomColors();
+        el.parentElement.style.backgroundColor = hexColor;
+        el.innerText = hexColor;
+    });
+});
+
+
 //to display time
 function displayTime() {
     // const days = ["SUN", "MON", "TUES", "WED", "THU", "FRI", "SAT"];
@@ -167,12 +147,12 @@ function displayTime() {
 };
 displayTime();
 
+
 // counter App
 function counterApp() {
     let counter = 0;
 
     document.getElementById("btn-increment").addEventListener("click", (e) => {
-        console.log(e);
         counter++;
         let increaseCounter = document.getElementById("count");
         increaseCounter.innerText = counter;
@@ -187,10 +167,32 @@ function counterApp() {
 counterApp();
 
 
-//Change the light bulb on mouse over and mouse leave
-const lightBulb = document.getElementById("dyk-lb");
-lightBulb.addEventListener("mouseover", () => lightBulb.setAttribute("src", "images/lb-on.png"));
-lightBulb.addEventListener("mouseleave", () => lightBulb.setAttribute("src", "images/lbg.png"));
+// password generator
+const generateRandomInt = (max, min) => Math.round(Math.random() * (max - min + 1)) + min;
+function passwordGenerator() {
+    let passwordLength = generateRandomInt(10, 15);
+    let password = '';
+    const characters = 'abcdefghijklmNOPQRSTUVWXYZ0123456789~!@#$%^&*()ABCDEFGHIJKLMnopqrstuvwxyz';
+    for (let i = 0; i < passwordLength; i++) {
+        const random = Math.round(Math.random() * characters.length);
+        password += characters.charAt(random);
+    }
+    return password;
+}
+
+function displayPassword() {
+    const genPassBtn = document.getElementById('btn-password');
+    genPassBtn.addEventListener('click', () => {
+        let password = passwordGenerator();
+        const passText = document.getElementById('pass-text');
+        const passLen = document.getElementById('pass-len');
+        passText.innerText = password;
+        passLen.innerText = password.length;
+
+    });
+}
+displayPassword();
+
 
 //to fetch random funfacts
 function getFunFacts() {
@@ -207,6 +209,12 @@ getFunFacts();
 setInterval(getFunFacts, 60000);
 
 document.getElementById("dyk-lb").addEventListener("click", () => getFunFacts());
+
+//Change the light bulb on mouse over and mouse leave
+const lightBulb = document.getElementById("dyk-lb");
+lightBulb.addEventListener("mouseover", () => lightBulb.setAttribute("src", "images/lb-on.png"));
+lightBulb.addEventListener("mouseleave", () => lightBulb.setAttribute("src", "images/lbg.png"));
+
 
 //to fetch JS Q&A - sequencial order
 let index = 0;
@@ -272,29 +280,3 @@ getQuotes();
 setInterval(getQuotes, 100000);
 
 document.getElementById("quote-btn").addEventListener("click", () => getQuotes());
-
-// password generator
-const generateRandomInt = (max, min) => Math.round(Math.random() * (max - min + 1)) + min;
-function passwordGenerator() {
-    let passwordLength = generateRandomInt(10, 15);
-    let password = '';
-    const characters = 'abcdefghijklmNOPQRSTUVWXYZ0123456789~!@#$%^&*()ABCDEFGHIJKLMnopqrstuvwxyz';
-    for (let i = 0; i < passwordLength; i++) {
-        const random = Math.round(Math.random() * characters.length);
-        password += characters.charAt(random);
-    }
-    return password;
-}
-
-function displayPassword() {
-    const genPassBtn = document.getElementById('btn-password');
-    genPassBtn.addEventListener('click', () => {
-        let password = passwordGenerator();
-        const passText = document.getElementById('pass-text');
-        const passLen = document.getElementById('pass-len');
-        passText.innerText = password;
-        passLen.innerText = password.length;
-
-    });
-}
-displayPassword();
